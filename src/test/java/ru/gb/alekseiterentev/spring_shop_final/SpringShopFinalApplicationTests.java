fa7377074b525245834b4b5ef06d82a004b9c0ec
Aleksei Terentev <aleksei.terentev@NetCracker.com>
2021-09-18 16:16:48 +0400

    Initial commit

---
+++ src/test/java/ru/gb/alekseiterentev/spring_shop_final/SpringShopFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.alekseiterentev.spring_shop_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
